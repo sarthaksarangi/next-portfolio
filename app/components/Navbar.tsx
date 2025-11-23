205439c359af4f326e6c141244fceb7977283ad8
sarthaksarangi <sarthakrohan5@gmail.com>
2025-11-23 17:25:01 +0530

    Removed unnecessary animations

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useEffect, useState } from "react";
 
 export const Navbar = () => {
   const [isScrolled, setIsScrolled] = useState(false);
